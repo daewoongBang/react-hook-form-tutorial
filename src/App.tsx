4cf655e2786904a2987a12c5cf2cb1856394b7e0
daewoongBang <daewoong.bang@gmail.com>
2020-01-16 18:41:56 +0900

    form 코드 작성 중

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
+type FormData = {
+  userName: string;
+};
+
 const App: React.FC = () => {
-  return <div>Hello</div>;
+  const { register, handleSubmit, errors } = useForm<FormData>();
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="userName">이름</label>
+      <input name="userName" placeholder="이름" ref={register({ required: '이름은 필수 항목 입니다.' })} />
+      {errors.userName && errors.userName.message}
+      <input type="submit" />
+    </form>
+  );
 };
 
 export default App;
